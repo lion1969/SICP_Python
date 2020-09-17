5c78b6f51c4d7396af507102bbeffa9294a792f5
Lion69 <lion69@mail.ru>
2020-09-17 17:37:14 +0300

    Change structure. Now examples would place in separate directories.

--- Chapter 1/1.5.5_Iteration.py
+++ Chapter 1/1.5.5_Iteration.py
@@ -5,7 +5,11 @@ def fib(n):
     k = 2              # position for curr in the sequence
 
     while k < n:
+        print("b4 asign.    pred = ", pred, "; curr = ", curr)
         pred, curr = curr, pred + curr # Re-bind pred and curr
+        print("after asign. pred = ", pred, "; curr = ", curr)
         k = k + 1
+        print("[||]")
     return curr
 
+print(fib(11))
